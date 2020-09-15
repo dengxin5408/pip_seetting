22a90d4cf639d6616df184190f7a1f6acb8c6deb
Deng Xin <181303@OC.COM>
2020-09-15 15:01:03 +0800

    'pip'

---
+++ laodeng-api-1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='laodeng',
+      version='0.1',
+      description='a test packcge',
+      url='http://github.com/tongling/clinicaltrial',
+      author='Ling',
+      author_email='tonglingacademic@gmail.com',
+      license='MIT',
+      packages=['clinicaltrial','filter'],
+      zip_safe=False)
